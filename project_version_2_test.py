79d4163e425b8dab319b287fe04d4198789a270b
Mohammadhossein Jafari <mohammadhosseinjafari95@gmail.com>
2022-08-09 14:39:50 +0200

    Update project_version_2_test.py
    Visualization of results in another file

--- project_version_2_test.py
+++ project_version_2_test.py
@@ -5,9 +5,6 @@ Created on Thu Aug  4 02:00:31 2022
 @author: unimi
 """
 from project_version_2 import *
-import unittest
-class Testproject(unittest.TestCase):
-     def test(self):
-        self.assertRaises(TypeError, vorarr, True)
+
         
        
