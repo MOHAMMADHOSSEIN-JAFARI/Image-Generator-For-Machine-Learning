adadb7b05f4837a96c71fd30008364171ef94740
Mohammadhossein Jafari <mohammadhosseinjafari95@gmail.com>
2022-08-12 23:29:06 +0200

    Update project_version_2_test.py
    #Information about the first generated image:

--- project_version_2_test.py
+++ project_version_2_test.py
@@ -6,7 +6,12 @@ Created on Thu Aug  4 02:00:31 2022
 """
 from project_version_2 import *
 plt.imshow(stored[0])
-print(list_torch)
+#Information about the first generated image: 
+print(list_torch[0])
+print(type(list_torch[0]))
+print(list_torch[0].size())
+print(list_torch[0].ndim)   
+
 
         
        
