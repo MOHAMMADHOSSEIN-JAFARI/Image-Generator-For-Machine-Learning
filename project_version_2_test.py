c8578ee1887c29382c6025bc8884d52318d9c4e0
Mohammadhossein Jafari <mohammadhosseinjafari95@gmail.com>
2022-08-19 16:58:49 +0200

    Update project_version_2_test.py

--- project_version_2_test.py
+++ project_version_2_test.py
@@ -5,13 +5,12 @@ Created on Thu Aug  4 02:00:31 2022
 @author: unimi
 """
 from project_version_2 import *
-plt.imshow(stored[0])
 #Information about the first generated image: 
 print(list_torch[0])
 print(type(list_torch[0]))
 print(list_torch[0].size())
 print(list_torch[0].ndim)   
-
+plt.imshow(list_torch[0])
 
         
        
